b805ec3f9bbf5abf45969253405e623d49afea4d
John Doan <doanja@plu.edu>
2019-09-15 01:14:12 -0700

    added render functions for rendering defender info, wip: need to hide all other character buttons, and display only the selected character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -52,8 +52,63 @@ const renderPlayerCards = (parentElement, character) => {
 
 const initClickListeners = (id) => {
   $('#' + id.name).click(function() {
-    console.log('button ' + id.name);
+    console.log($('#' + id.name));
   });
+};
+
+const renderDefenderCard = (parentElement, character) => {
+  const col = $('<div>', { class: 'col-sm-12 col-md-3 ' + character.name });
+  const card = $('<div>', { class: 'card defender' + character.name });
+  const cardBody = $('<div>', { class: 'card-body ' + character.name });
+  const cardTitle = $('<h5>', {
+    class: 'card-title text-center'
+  }).text(character.name);
+  const img = $('<img>', {
+    class: 'mx-auto ' + character.name,
+    src: 'https://picsum.photos/200',
+    alt: 'sample image'
+  });
+  const p = $('<p>', { class: 'card-text ' + character.name }).text(
+    character.hp + ' HP'
+  );
+
+  parentElement.append(col);
+  col.append(card);
+  card.append(cardBody);
+  cardBody.append(cardTitle);
+  cardBody.append(img);
+  cardBody.append(p);
+};
+
+const renderFightOptions = (parentElement) => {
+  const col = $('<div>', { class: 'col-sm-12 col-md-3' });
+  const card = $('<div>', { class: 'card defender' });
+  const cardBody = $('<div>', { class: 'card-body' });
+  const cardTitle = $('<h5>', {
+    class: 'card-title text-center'
+  }).text('Fight Options');
+  const button = $('<button>', {
+    class: 'btn btn-warning w-100',
+    id: 'fight_button'
+  }).text('Fight');
+
+  parentElement.append(col);
+  col.append(card);
+  card.append(cardBody);
+  cardBody.append(cardTitle);
+  cardBody.append(button);
+};
+
+const renderGameStatus = (parentElement) => {
+  const col = $('<div>', { class: 'col-sm-12 col-md-6' });
+  const card = $('<div>', { class: 'card defender' });
+  const cardBody = $('<div>', { class: 'card-body' });
+  const p = $('<p>', { class: 'card-text text-left' }).text('he he');
+
+  parentElement.append(col);
+  col.append(card);
+  card.append(cardBody);
+  cardBody.append(p);
 };
 
 /*
@@ -72,15 +127,14 @@ const initialize = () => {
     renderPlayerCards($('#player_row'), character);
     initClickListeners(character);
   });
+
+  renderDefenderCard($('#defender_row'), characters[0]);
+  renderFightOptions($('#defender_row'));
+  renderGameStatus($('#defender_row'));
 };
 
 window.onload = () => {
   initialize();
-
-  // renderPlayerCards($('#player_row'), kenobi);
-  // renderPlayerCards($('#player_row'), luke);
-  // renderPlayerCards($('#player_row'), sidious);
-  // renderPlayerCards($('#player_row'), maul);
 
   // $('.kenobi').on('click', function() {
   //   // character = initPlayer('fred', 100, 10, 5);
@@ -95,6 +149,5 @@ window.onload = () => {
   // });
 
   // console.log('kenobi', $('.kenobi')[4]);
-
   // console.log('special', $('#special'));
 };
