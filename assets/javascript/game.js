621817321e9d6e84d588c6157b9ac00b50c33692
John <spandexerjd@gmail.com>
2019-09-14 22:58:57 -0700

    finished render card function, wip on click functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,74 @@
 // globals
 let kenobi, luke, sidious, maul;
 
-function Character(name, healthPts, attackPts, counterPts) {
+function Character(name, hp, ap, cap) {
   this.name = name;
-  this.hp = healthPts;
-  this.ap = attackPts;
-  this.cap = counterPts;
+  this.hp = hp;
+  this.ap = ap;
+  this.cap = cap;
 }
 
-const initPlayer = (name, hp, ap, cap) => {
-  let character = new Character(name, hp, ap, cap);
-  return character;
+const initialize = () => {
+  // foor loop - go through each player jquery, go to array, set each of the elements from the array
+  kenobi = new Character('kenobi', 120, 5, 7);
+  luke = new Character('luke', 100, 3, 8);
+  sidious = new Character('sidious', 150, 10, 3);
+  maul = new Character('maul', 180, 19, 2);
 };
 
-// const initialize = () => {
-//   // foor loop - go through each player jquery, go to array, set each of the elements from the array
-//   kenobi = new Character(kenobi, 120, 5, 7);
-//   luke = new Character(luke, 100, 3, 8);
-//   sidious = new Character(sidious, 150, 10, 3);
-//   maul = new Character(maul, 180, 19, 2);
-// };
+const renderPlayerCards = (parentElement, character) => {
+  const col = $('<div>', { class: 'col-sm-12 col-md-3 ' + character.name });
+  const card = $('<div>', { class: 'card player' + character.name });
+  const cardBody = $('<div>', { class: 'card-body ' + character.name });
+  const cardTitle = $('<h5>', {
+    class: 'card-title text-center'
+  }).text(character.name);
+  const img = $('<img>', {
+    class: 'mx-auto ' + character.name,
+    src: 'https://picsum.photos/200',
+    alt: 'sample image'
+  });
+  const p = $('<p>', { class: 'card-text ' + character.name }).text(
+    character.hp + ' HP'
+  );
+  const button = $('<button>', {
+    class: 'btn btn-primary w-100 ' + character.name,
+    id: 'special'
+  }).text('Select');
 
-const renderPlayerCards = (parent, i) => {
-  const col = $('<div>', { class: 'col-sm-12 col-md-3' });
-  const card = $('<div>', { class: 'card' });
-  const cardBody = $('<div>', { class: 'card-body' });
-  const cardTitle = $('<h5>', { class: 'card-title text-center' });
+  parentElement.append(col);
+  col.append(card);
+  card.append(cardBody);
+  cardBody.append(cardTitle);
+  cardBody.append(img);
+  cardBody.append(p);
+  cardBody.append(button);
 };
 
 window.onload = () => {
-  // $('#select_player_1').on('click', function() {
-  //   character = initPlayer('fred', 100, 10, 5);
+  initialize();
+
+  renderPlayerCards($('#player_row'), kenobi);
+  // renderPlayerCards($('#player_row'), luke);
+  // renderPlayerCards($('#player_row'), sidious);
+  // renderPlayerCards($('#player_row'), maul);
+
+  // $('.kenobi').on('click', function() {
+  //   // character = initPlayer('fred', 100, 10, 5);
   //   $('.player').hide();
   //   $('#player_card_1').show();
-  //   $('#player_name_1').text(character.name);
-  //   $('#player_hp_1').text(character.hp + ' HP');
+  //   // $('#player_name_1').text(character.name);
+  //   // $('#player_hp_1').text(character.hp + ' HP');
   // });
-  // $('#select_player_2').on('click', function() {
-  //   character = initPlayer('bob', 200, 5, 7);
+
+  // $('.kenobi').on('click', function() {
   //   $('.player').hide();
-  //   $('#player_card_2').show();
   // });
+
+  // console.log($('.kenobi')[4]);
+  console.log(
+    $('#special').click(function() {
+      console.log('he');
+    })
+  );
 };
