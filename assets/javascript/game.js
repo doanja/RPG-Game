44c406341dd047f7ac515a347d80dbb528c6dcd3
John <spandexerjd@gmail.com>
2019-09-16 09:46:06 -0700

    updated fight button functionality: updates defender card with the selected enemy and players stats, checked for win/lose conditions, seperated initialize function into two functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ const characters = []; // used to store Character objects
 let kenobi, luke, sidious, maul; // characters
 let selectedCharacter, selectedDefender;
 let canSelectEnemy; // change this to false when you click on an enemy,
+let enemiesRemaining;
 
 /*
  * @param name, the name of the character
@@ -11,11 +12,12 @@ let canSelectEnemy; // change this to false when you click on an enemy,
  * @param cap, the counter attack points of the character
  * Constructor for a character
  */
-function Character(name, hp, ap, cap) {
+function Character(name, hp, ap, cap, baseAp) {
   this.name = name;
   this.hp = hp;
   this.ap = ap;
   this.cap = cap;
+  this.baseAp = baseAp;
 }
 
 /*
@@ -38,7 +40,9 @@ const renderCards = (parentElement, character, str, buttonColor) => {
     src: 'https://picsum.photos/200',
     alt: 'sample image'
   });
-  const p = $('<p>', { class: 'card-text' }).text(character.hp + ' HP');
+  const p = $('<p>', { class: 'card-text ' + str + '-health' }).text(
+    character.hp + ' HP'
+  );
   const button = $('<button>', {
     class: 'btn btn-' + buttonColor + ' w-100',
     id: character.name + '-button-' + str
@@ -59,43 +63,59 @@ const renderCards = (parentElement, character, str, buttonColor) => {
  * function that adds click listeners to each player card and hides
  * all the other player cards except for the one selected
  */
-const onPlayerSelect = (character) => {
+const onPlayerSelect = character => {
   $('#' + character.name + '-button-player').click(function() {
     $('.player').hide();
     $('.' + character.name + '-player').show();
 
-    updateCurrentPlayer(character); // update global selectedCharacter
+    selectedCharacter = { ...character };
+    // // console.log('selectedCharacter :', selectedCharacter);
+    // updateCurrentPlayer(character); // update global selectedCharacter
   });
 };
 
-const onEnemySelect = (character) => {
+const onEnemySelect = character => {
   $('#' + character.name + '-button-enemy').click(function() {
-    updateCurrentDefender(character);
+    if (!canSelectEnemy) {
+      alert('An enemy has already been selected.');
+    } else {
+      canSelectEnemy = false;
+      enemiesRemaining--;
+      $('.' + character.name + '-enemy').hide();
 
-    // render defender info
-    renderDefenderCard($('#defender_row'), selectedDefender); // update this with selectedDefender
-    renderFightOptions($('#defender_row'));
-    renderGameStatus($('#defender_row'));
+      selectedDefender = { ...character };
+      // console.log('selectedDefender :', selectedDefender);
+      // updateCurrentDefender(character);
 
-    onFight();
+      // render defender info
+      renderDefenderCard($('#defender_row'), selectedDefender); // update this with selectedDefender
+      renderFightOptions($('#defender_row'));
+      renderGameStatus($('#defender_row'), selectedDefender);
+
+      // console.log('enemy selected...');
+
+      fightButtonListener();
+    }
   });
 };
 
 const renderDefenderCard = (parentElement, character) => {
-  const col = $('<div>', { class: 'col-sm-12 col-md-3 ' + character.name });
-  const card = $('<div>', { class: 'card defender' + character.name });
-  const cardBody = $('<div>', { class: 'card-body ' + character.name });
+  const col = $('<div>', { class: 'col-sm-12 col-md-3' });
+  const card = $('<div>', {
+    class: 'card-defender'
+  });
+  const cardBody = $('<div>', { class: 'card-body' });
   const cardTitle = $('<h5>', {
-    class: 'card-title text-center'
+    class: 'card-title text-center defender-title'
   }).text(character.name);
   const img = $('<img>', {
-    class: 'mx-auto ' + character.name,
+    class: 'mx-auto defender-image',
     src: 'https://picsum.photos/200',
     alt: 'sample image'
   });
-  const p = $('<p>', { class: 'card-text ' + character.name }).text(
-    character.hp + ' HP'
-  );
+  const p = $('<p>', {
+    class: 'card-text defender-hp'
+  }).text(character.hp + ' HP');
 
   parentElement.append(col);
   col.append(card);
@@ -105,9 +125,9 @@ const renderDefenderCard = (parentElement, character) => {
   cardBody.append(p);
 };
 
-const renderFightOptions = (parentElement) => {
+const renderFightOptions = parentElement => {
   const col = $('<div>', { class: 'col-sm-12 col-md-3' });
-  const card = $('<div>', { class: 'card defender' });
+  const card = $('<div>', { class: 'card-defender' });
   const cardBody = $('<div>', { class: 'card-body' });
   const cardTitle = $('<h5>', {
     class: 'card-title text-center'
@@ -124,12 +144,12 @@ const renderFightOptions = (parentElement) => {
   cardBody.append(button);
 };
 
-const renderGameStatus = (parentElement) => {
+const renderGameStatus = (parentElement, defender) => {
   const col = $('<div>', { class: 'col-sm-12 col-md-6' });
-  const card = $('<div>', { class: 'card defender' });
+  const card = $('<div>', { class: 'card-defender' });
   const cardBody = $('<div>', { class: 'card-body' });
   const p = $('<p>', { class: 'card-text text-left', id: 'game-status' }).text(
-    'he he'
+    'Now fighting ' + defender.name + '.'
   );
 
   parentElement.append(col);
@@ -138,13 +158,52 @@ const renderGameStatus = (parentElement) => {
   cardBody.append(p);
 };
 
-const onFight = () => {
+const fightButtonListener = () => {
+  // console.log('fightButtonListener called...');
+  // parameter for msg?
+  console.log('enemiesRemaining :', enemiesRemaining);
   $('#fight-button').click(function() {
-    // console.log('fight-button was clicked');
+    // console.log('fight button clicked');
+
+    /* update defender info */
+
     // update player hp, ap
-    // update defender hp
+    selectedCharacter.hp -= selectedDefender.cap; // decrease player's hp base on defender's counter attack power
+    selectedDefender.hp -= selectedCharacter.ap; // decrease defender's hp base on player's attack power
+    selectedCharacter.ap += selectedCharacter.baseAp; // increasse player's base attack power
+
+    // console.log('PLAYER HP :', selectedCharacter.hp);
+    // console.log('ENEMY HP :', selectedDefender.hp);
+    // console.log('PLAYER ATTACK :', selectedCharacter.ap);
+    // console.log('----------------------------------------------------');
+
+    $('.player-health').text(selectedCharacter.hp + ' HP');
+    $('.defender-hp').text(selectedDefender.hp + ' HP');
+
     // re-render player and defener hp
-    $('#game-status').text('some text here was updated');
+    $('#game-status').text(
+      'You attacked ' +
+        selectedDefender.name +
+        ' for ' +
+        selectedCharacter.ap +
+        ' damage. ' +
+        selectedDefender.name +
+        ' attacked you back for ' +
+        selectedDefender.cap +
+        ' damage.'
+    );
+    /* check battle status */
+    if (selectedCharacter.hp <= 0) {
+      alert('gameover');
+      location.reload();
+    } else if (enemiesRemaining === 0 && selectedDefender.hp <= 0) {
+      alert('u win');
+      location.reload();
+    } else if (selectedDefender.hp <= 0) {
+      alert('defender hp under 0');
+      $('.card-defender').remove();
+      canSelectEnemy = true;
+    }
   });
 };
 
@@ -152,36 +211,34 @@ const onFight = () => {
  * initializes four Characters and adds them to a characters array.
  * renders the elements and adds click listeners to each card
  */
-const initialize = () => {
+const initializeVariables = () => {
+  canSelectEnemy = true;
+  enemiesRemaining = 0;
+
   // initialize four characters
-  kenobi = new Character('Kenobi', 120, 5, 7);
-  luke = new Character('Luke', 100, 3, 8);
-  sidious = new Character('Sidious', 150, 10, 3);
-  maul = new Character('Maul', 180, 19, 2);
+  kenobi = new Character('Kenobi', 120, 5, 7, 5);
+  luke = new Character('Luke', 100, 3, 8, 3);
+  sidious = new Character('Sidious', 150, 10, 3, 10);
+  maul = new Character('Maul', 180, 19, 2, 19);
 
   // add the four characters to the array
   characters.push(kenobi, luke, sidious, maul);
+};
 
+const initializePage = () => {
   // loop through the array and render characters and add click listeners
-  characters.forEach((character) => {
+  characters.forEach(character => {
+    enemiesRemaining++;
     renderCards($('#player_row'), character, 'player', 'primary');
     onPlayerSelect(character);
 
     renderCards($('#enemy_row'), character, 'enemy', 'danger');
     onEnemySelect(character);
   });
-};
-
-const updateCurrentPlayer = (character) => {
-  selectedPlayer = { ...character }; //
-  console.log('selectedPlayer :', selectedPlayer);
-};
-
-const updateCurrentDefender = (character) => {
-  selectedDefender = { ...character }; //
-  console.log('selectedDefender :', selectedDefender);
+  console.log('enemiesRemaining :', enemiesRemaining);
 };
 
 window.onload = () => {
-  initialize();
+  initializeVariables();
+  initializePage();
 };
